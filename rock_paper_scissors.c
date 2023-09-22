fc578cd9342d07da2b027aedc8ef74d5c5d3b384
Neeraj Chormale <neerajchormale39@gmail.com>
2023-09-22 23:59:38 +0530

    the code was optimised as the logic was optimized by writing in shorter format

--- rock_paper_scissors.c
+++ rock_paper_scissors.c
@@ -1,31 +1,17 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 #include <time.h>
 
 int game(char you,char computer)
 {
     if (you==computer)
+        return 0;
+
+    if ((you=='s' && computer=='p')||(you == 'p'&& computer =='z')||
+        (you=='z'&& computer =='s'))
         return -1;
-
-    if (you=='s' && computer=='p')
-        return 0;
     
-    else if (you=='p' && computer=='s')
-        return 1;
-
-    if (you=='s' && computer=='z')
-        return 1;
-    
-    else if (you=='z' && computer=='s')
-        return 0;
-    
-    if (you=='p' && computer=='z')
-        return 0;
-
-    else if (you=='z'&& computer=='p')
-        return 1;
-   
+    return 1;
 }
 
 int main()
